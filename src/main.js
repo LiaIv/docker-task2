0a4b15375f9bcccc137032520a7c7ba467b9486f
MinkY <minky@deeplay.io>
2026-02-01 00:05:19 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <main class="container">
+    <h1>Виртуализация и контейнеризация</h1>
+    <p>Это простое фронтенд-приложение, которое собирается в статику.</p>
+  </main>
+`;
